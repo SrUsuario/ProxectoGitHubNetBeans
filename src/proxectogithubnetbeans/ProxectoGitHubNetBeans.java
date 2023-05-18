e8e4f31c726a812738089e37100aa9482e615d83
SrUsuario <raul.parada040103@gmail.com>
2023-05-18 18:23:43 +0200

    Update ProxectoGitHubNetBeans

--- src/proxectogithubnetbeans/ProxectoGitHubNetBeans.java
+++ src/proxectogithubnetbeans/ProxectoGitHubNetBeans.java
@@ -15,6 +15,7 @@ public class ProxectoGitHubNetBeans {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Esto sale pola pantalla");
     }
     
 }
